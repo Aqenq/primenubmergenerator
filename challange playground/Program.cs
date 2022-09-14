890b389eea477879b2c0c8d385ac072eefbc58f9
pc <pc@DESKTOP-LPQDDUP>
2022-09-14 16:23:23 +0300

    Proje dosyası ekle.

---
+++ challange playground/Program.cs
@@ -0,0 +1,28 @@
+﻿int sayi1;
+sayi1 = int.Parse(Console.ReadLine());
+int sayi2 = sayi1 - 1;
+a:
+while (true)
+{
+
+    while (sayi1 % sayi2 != 0)
+    {
+        sayi2 = sayi2 - 1;
+    }
+    if (sayi2 == 1)
+        {
+            Console.WriteLine(sayi1 + "sayı asaldır");
+            break;
+        }
+    if (sayi1 % sayi2 == 0)
+{  
+        break;
+}
+}
+sayi1++;
+sayi2 = sayi1 - 1;
+goto a;
+    
+    
+    
+    
